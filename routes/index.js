3a4d91f1277f82a3381b12539e03149c070c88a1
Oscar Garcia Juarez <ogarcia@pleedtech.com>
2021-02-12 18:44:54 -0600

    add headers

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const router = Router();
 
 router.post('/login', cors(),async(req, res, next) => {
     try{
+        res.header("Access-Control-Allow-Origin", "https://zen-jones-87ccd3.netlify.app/"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     const response = await axios.post('http://ec2-52-37-61-68.us-west-2.compute.amazonaws.com:1234/api/v1/challenge/login',
     {user: req.body.email, password: req.body.password}
     )
